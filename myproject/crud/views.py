af4217aad657b1486f5f4570e11cbd5fe7ec8868
asagao0414 <150596498+asagao0414@users.noreply.github.com>
2024-02-02 23:59:30 +0900

    Add files via upload

---
+++ myproject/crud/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from .models import Product
+from django.urls import reverse_lazy
+
+# Create your views here.
+class TopView(TemplateView):
+  template_name = "top.html"
+
+class ProductListView(ListView):
+  model = Product
+  paginate_by = 3
+
+class ProductCreateView(CreateView):
+  model = Product
+  fields = '__all__'
+
+class ProductDetailView(DetailView):
+  model = Product
+  template_name = 'crud/product_detail.html'
